460312a460df6347ae4be6dd80bcdb9c454d063d
devpark <devpark29@gmail.com>
2024-02-06 18:32:31 +0900

    change element to text in store

--- src/components/sidemenu.tsx
+++ src/components/sidemenu.tsx
@@ -5,6 +5,10 @@ import {
   ChartBarIcon,
   MagnifyingGlassIcon,
   HomeIcon,
+  CalendarIcon,
+  BookmarkSlashIcon,
+  BriefcaseIcon,
+  BuildingLibraryIcon,
 } from "@heroicons/react/20/solid";
 
 // Redux state
@@ -23,6 +27,14 @@ export default function SideMenu() {
 
   let dispatch = useDispatch();
 
+  //아이콘 맵핑
+  const icons: { [key: string]: React.ReactElement } = {
+    CalendarIcon: <CalendarIcon className="h-5 w-5" />,
+    BookmarkSlashIcon: <BookmarkSlashIcon className="h-5 w-5" />,
+    BriefcaseIcon: <BriefcaseIcon className="h-5 w-5" />,
+    BuildingLibraryIcon: <BuildingLibraryIcon className="h-5 w-5" />,
+  };
+
   //Dropdown Status 상태 관리
   let [ddStatus, setDdStatus] = useState(false);
   useEffect(() => {
@@ -40,11 +52,24 @@ export default function SideMenu() {
   }, [ddStatus]);
 
   //메뉴 검색
-  const [searchInput, setSearchInput] = useState("");
-  const searchValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchInput(e.target.value.toLowerCase());
-    console.log(searchInput);
+  type NavigationItem = {
+    category: number;
+    name: string;
+    href: string;
+    current: boolean;
+    icon: null | string;
   };
+
+  const [searchValue, setSearchValue] = useState("");
+  const [searched, setSearched] = useState<NavigationItem[]>([]);
+
+  useEffect(() => {
+    const result: NavigationItem[] = navigation.filter((item) =>
+      item.name.toLowerCase().includes(searchValue.toLowerCase()),
+    );
+    console.log(result);
+    setSearched(result);
+  }, [searchValue]);
 
   return (
     <div
@@ -67,79 +92,81 @@ export default function SideMenu() {
             type="text"
             placeholder="Menu Search"
             className="text-[15px] ml-4 w-full bg-transparent focus:outline-none"
-            onChange={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
           />
         </div>
-        <div className="my-2 bg-gray-600 h-[1px]"></div>
       </div>
-      <Disclosure defaultOpen={true}>
-        {({ open }) => (
-          <>
-            <Disclosure.Button
-              onClick={() => setDdStatus(!ddStatus)}
-              className="flex w-full justify-between px-4 py-4 text-left text-sm font-medium text-gray-100 hover:bg-gray-700 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75"
-            >
-              <div className="flex">
-                <ChartBarIcon className="h-5 w-5" />
-                <span className="ml-2">Dropdown Menu</span>
-              </div>
-              <ChevronUpIcon
-                className={`${
-                  ddStatus ? "rotate-180 transform" : ""
-                } h-5 w-5 text-gray-500`}
-              />
-            </Disclosure.Button>
-            {ddStatus && (
-              <div className="px-4 text-sm text-gray-500 flex flex-col">
-                {navigation
-                  .filter((item) => item.category === 1)
-                  .map((item, index) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className={`${
-                        item.current
-                          ? "bg-gray-900 text-gray-100"
-                          : "text-gray-100 hover:bg-gray-700 hover:text-gray-100"
-                      } px-3 py-2 text-sm font-medium`}
-                      onClick={() => dispatch(changeCurrent(index))}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-              </div>
-            )}
-            {/* 일반 버튼 메뉴 */}
-            <div className="my-2 bg-gray-600 h-[1px]" />
-            {navigation
-              .filter((item) => item.category === 2)
-              .map((item, index) => {
-                return (
-                  <a
-                    key={item.name}
-                    href={item.href}
+      {false ? (
+        <></>
+      ) : (
+        <>
+          <div className="my-2 bg-gray-600 h-[1px]"></div>
+          <Disclosure defaultOpen={true}>
+            {({ open }) => (
+              <>
+                <Disclosure.Button
+                  onClick={() => setDdStatus(!ddStatus)}
+                  className="flex w-full justify-between px-4 py-4 text-left text-sm font-medium text-gray-100 hover:bg-gray-700 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75"
+                >
+                  <div className="flex">
+                    <ChartBarIcon className="h-5 w-5" />
+                    <span className="ml-2">Dropdown Menu</span>
+                  </div>
+                  <ChevronUpIcon
                     className={`${
-                      item.current
-                        ? "bg-gray-900 text-gray-100"
-                        : "text-gray-100 hover:bg-gray-700 hover:text-gray-100"
-                    } flex space-x-3 px-4 py-4 text-sm text-gray-100 hover:bg-gray-700 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75`}
-                    onClick={() => dispatch(changeCurrent(index + 4))}
-                    aria-current={item.current ? "page" : undefined}
-                  >
-                    <>
-                      {item.icon &&
-                        React.cloneElement(item.icon, {
-                          className: "h-5 w-5",
-                        })}
-                    </>
-                    <div>{item.name}</div>
-                  </a>
-                );
-              })}
-          </>
-        )}
-      </Disclosure>
+                      ddStatus ? "rotate-180 transform" : ""
+                    } h-5 w-5 text-gray-500`}
+                  />
+                </Disclosure.Button>
+                {ddStatus && (
+                  <div className="px-4 text-sm text-gray-500 flex flex-col">
+                    {navigation
+                      .filter((item) => item.category === 1)
+                      .map((item, index) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={`${
+                            item.current
+                              ? "bg-gray-900 text-gray-100"
+                              : "text-gray-100 hover:bg-gray-700 hover:text-gray-100"
+                          } px-3 py-2 text-sm font-medium`}
+                          onClick={() => dispatch(changeCurrent(index))}
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                  </div>
+                )}
+                {/* 일반 버튼 메뉴 */}
+                <div className="my-2 bg-gray-600 h-[1px]" />
+                {navigation
+                  .filter((item) => item.category === 2)
+                  .map((item, index) => {
+                    return (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={`${
+                          item.current
+                            ? "bg-gray-900 text-gray-100"
+                            : "text-gray-100 hover:bg-gray-700 hover:text-gray-100"
+                        } flex space-x-3 px-4 py-4 text-sm text-gray-100 hover:bg-gray-700 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75`}
+                        onClick={() => dispatch(changeCurrent(index + 4))}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        <>{item.icon ? icons[item.icon] : null}</>
+                        <div>{item.name}</div>
+                      </a>
+                    );
+                  })}
+                {/* 검색 결과 */}
+              </>
+            )}
+          </Disclosure>
+        </>
+      )}
     </div>
   );
 }
